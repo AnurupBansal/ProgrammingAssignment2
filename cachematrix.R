dc7ad58c405747253ad4b3a8157cf7153d39382b
Anurup Bansal <anurupbansal@gmail.com>
2020-06-24 01:39:20 +0530

    Completed

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,21 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      x <<- x;                             ## creates a global variable x with the matrix in it
+      m <<- NULL;                          ## creates a global variable m with value NULL
+      x                                    ## this value is returned
+      
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      if(!is.null(m)){                     ## checks in the global environment for the value of m
+            message("Data Found....")
+            return(m)
+      }
+      m <<- solve(x)                       ## if not found , then it solves it
+      m
+      ## Return a matrix that is the inverse of 'x'
 }
